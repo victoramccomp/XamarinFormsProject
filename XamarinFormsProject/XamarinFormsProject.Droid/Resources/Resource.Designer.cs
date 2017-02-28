fd37ce85d6d549b14a30d6c4206b097f283bc488
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2017-02-28 15:28:53 -0300

    Update Xamarin.Forms -> 2.3.3.193

--- XamarinFormsProject/XamarinFormsProject.Droid/Resources/Resource.Designer.cs
+++ XamarinFormsProject/XamarinFormsProject.Droid/Resources/Resource.Designer.cs
@@ -26,7 +26,6 @@ namespace XamarinFormsProject.Droid
 		
 		public static void UpdateIdValues()
 		{
-			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::XamarinFormsProject.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
