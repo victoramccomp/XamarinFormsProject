371fb045ed069bad906473f525383bded51273a4
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2017-02-28 15:44:04 -0300

    Packages Microsoft.Net.Http on portable; NewtonSoft JSON; Class Cat created at Models

---
+++ XamarinFormsProject/XamarinFormsProject/Models/Cat.cs
@@ -0,0 +1,12 @@
+﻿namespace XamarinFormsProject.Models
+{
+    public class Cat
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string WebSite { get; set; }
+        public string Image { get; set; }
+    }
+}
